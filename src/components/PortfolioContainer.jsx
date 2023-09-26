e48be5ef8db52000d1b823de8c96863347f6d852
danmac121 <danmacdonald121@gmail.com>
2023-09-25 20:51:41 -0400

    added pages, conditional rendering, footer component

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import NavTabs from './NavTabs';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import Footer from './Footer';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+    
+      <main className="mx-3">{renderPage()}</main>
+      <Footer />
+    </div>
+  );
+
+ 
+}
