7b3cce480658018dc4c10277c99531d18fc96ca4
danmac121 <danmacdonald121@gmail.com>
2023-09-27 21:21:52 -0400

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
+import './footer.css'
 const Footer = () => {
     return (
         <footer>
-            <p></p>
-           
+             <a href="https://github.com/danmac121"  className="">
+                 
+                 <img src="./assets/git.png" alt="GitHub Repo" />
+             </a>
+             <a href="https://www.linkedin.com/in/daniel-macdonald-289946280/"  className="">
+                 
+                 <img src="./assets/linked.png" alt="GitHub Repo" />
+             </a>
+             <a href="https://stackoverflow.com/users/22648176/dan-mac"  className="">
+                 
+                 <img src="./assets/stack.png" alt="GitHub Repo" />
+             </a>
         </footer>
     );
 };
