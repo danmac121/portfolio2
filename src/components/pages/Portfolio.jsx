4b4391829e28812b130a687fe144c980122c6ea5
danmac121 <danmacdonald121@gmail.com>
2023-09-27 20:34:45 -0400

    added portfolio page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,19 +1,85 @@
-export default function Portfolio() {
+import './Portfolio.css';
+
+export default function ProjectBox({ backgroundImage, liveSiteURL, githubURL }) {
   return (
-    <div className="portfolioContainer">
-      <h1>Portfolio</h1>
-      <div className="imageContainer">
-        <div className="project num1">
-          <a href=""><img src="" alt="" /></a>
-          <a href=""><img src="" alt="" /></a>
-        </div>
-        <div  className="project num2"></div>
-        <div  className="project num3"> </div>
-        <div  className="project num4"></div>
-        <div  className="project num5"></div>
+    <div className="mainContainer">
+      <h1>Previous Projects</h1>
+    <div className = "portfolioContainer">
+      
 
-
+      <div className="project-box" style={{ backgroundImage: `url("./assets/hopper.png")` }}>
+          <div className="project-overlay">
+              <a href={liveSiteURL}  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/brian-wastle/Channel-Hopper"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
       </div>
-    </div>
+      <div className="project-box" style={{ backgroundImage: `url("./assets/note.png")` }}>
+          <div className="project-overlay">
+              <a href="https://radiant-ravine-01589-dee562bfc599.herokuapp.com"  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/danmac121/NoteApp"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
+      </div>
+      <div className="project-box" style={{ backgroundImage: `url("./assets/jate.png")` }}>
+          <div className="project-overlay">
+              <a href="https://lit-caverns-97353-16e931334f59.herokuapp.com"  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/danmac121/textEditor"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
+      </div>
+      <div className="project-box" style={{ backgroundImage: `url("./assets/p1.png")` }}>
+          <div className="project-overlay">
+              <a href="https://pb1983.github.io/Project1/"  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/danmac121/Project1"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
+      </div>
+      <div className="project-box" style={{ backgroundImage: `url("./assets/weather.png")` }}>
+          <div className="project-overlay">
+              <a href="https://danmac121.github.io/WeatherChecker/"  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/danmac121/WeatherChecker"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
+      </div>
+      <div className="project-box" style={{ backgroundImage: `url("./assets/portfolio.png")` }}>
+          <div className="project-overlay">
+              <a href={liveSiteURL}  className="project-link live-site">
+                 
+                  <img src="./assets/circle.png" alt="Live Site" />
+              </a>
+              <a href="https://github.com/danmac121/portfolio2"  className="project-link github">
+                 
+                  <img src="./assets/github.png" alt="GitHub Repo" />
+              </a>
+          </div>
+      </div>
+      </div></div>
   );
 }
+
